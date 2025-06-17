5f3ee5b1391307ae4b46adfc6965edba5a5ec564
TranKiemHieu <tkiemhieu@gmail.com>
2025-06-17 09:06:01 +0700

    update .env dev and prod

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -1,4 +1,7 @@
 import { Options } from 'sequelize';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV || 'production'}` });
 
 const config: Options & { defaultPageSize: number; pool: NonNullable<Options["pool"]> } = {
     username: process.env.DB_USER || 'root',
@@ -13,6 +16,7 @@ const config: Options & { defaultPageSize: number; pool: NonNullable<Options["po
         idle: 10000,
     },
     defaultPageSize: parseInt(process.env.DEFAULT_PAGE_SIZE || '5'),
+    logging: process.env.NODE_ENV === 'development' ? console.log : false, // Enable logging only in development
 };
 
 export default config;
